8184e02c651ed45e2a348ec53902b24565d5d59d
Joey McKenzie <joey.mckenzie27@gmail.com>
2022-08-20 17:44:14 -0700

    feat(workspace): add code quality tools

---
+++ libs/conduit-api/feature-auth/README.md
@@ -0,0 +1,7 @@
+# conduit-api-feature-auth
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test conduit-api-feature-auth` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/conduit-api/feature-auth/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'conduit-api-feature-auth',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/libs/conduit-api/feature-auth',
+};
