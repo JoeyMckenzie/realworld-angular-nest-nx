500306764d9dc990f4245b1e87e212ba689acd8f
Joey McKenzie <joey.mckenzie27@gmail.com>
2022-08-20 17:53:26 -0700

    feat(prisma): add data model scaffold

--- apps/conduit-api/src/app/app.module.ts
+++ apps/conduit-api/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConduitApiFeatureAuthModule } from '@realworld-angular-nest-nx/conduit-api/feature-auth';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConduitApiFeatureAuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
